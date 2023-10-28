52718e6d3ac1b31f58591a83b3950640706bc401
Pankaj biradar <pankaj28988@gmail.com>
2023-10-28 22:06:55 +0530

    Add day4.py

---
+++ Day 4/day4.py
@@ -0,0 +1,18 @@
+''' Point class which compares two points and returns true if they are equal and false if they are not equal.'''
+
+class Point:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self,other):
+        return self.x == other.x and self.y == other.y
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
+    
+''' Instantiate two points and compare them.'''
+point1  = Point(1,2)
+point2 = Point(1,3)
+print(point1 == point2)
