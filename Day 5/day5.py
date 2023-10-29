d9037197e76b90bb5a5058ebd1206fa75dcb000f
Pankaj biradar <pankaj28988@gmail.com>
2023-10-29 22:44:52 +0530

    Add day5 task

---
+++ Day 5/day5.py
@@ -0,0 +1,6 @@
+""" Python program that reads a tet file and counts the number of lines in it."""
+
+with open("data.txt", "r") as test_file:
+    for count, line in enumerate(test_file):
+        pass
+print("Total number of files: ", count + 1)
