ad4e5ae5268f5296eec93f64934fea9d01b9215e
Pankaj biradar <pankaj28988@gmail.com>
2023-10-30 21:56:55 +0530

    Updates to day5.py

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,6 +1,10 @@
 """ Python program that reads a tet file and counts the number of lines in it."""
 
-with open("data.txt", "r") as test_file:
-    for count, line in enumerate(test_file):
-        pass
-print("Total number of files: ", count + 1)
+'''I have used exception handling to handle the file not found error. If the file is not found, the except block will be executed and the error message will be printed.'''
+try:
+    with open("data.txt", "r") as test_file:
+        for count, line in enumerate(test_file):
+            pass
+    print("Total number of files: ", count + 1)
+except FileNotFoundError:
+    print("File not found")
