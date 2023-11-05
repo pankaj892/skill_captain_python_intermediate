0bd5f2e5aa8604d595d29abeae328fa4221db11b
Pankaj biradar <pankaj28988@gmail.com>
2023-11-05 12:21:02 +0530

    Updates to day6.py

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,1 +1,33 @@
 ''' This program writes to a file programatically using a list in Python'''
+
+# Import the library to be used
+import random
+
+# Create a list of syllables to be used
+syllables = [
+    'mon','fay','shi','zag','blarg','rash','izen','diz','kiz','zay','zod','zak','zaz','zi'
+]
+
+
+# Create a function to generate a name
+def generate_name(used_names):
+    name = random.choice(syllables) + random.choice(syllables) + random.choice(syllables)
+    name.capitalize()
+    if name in used_names:
+        return generate_name(used_names)
+    used_names.add(name.capitalize())
+    return name.capitalize()
+
+# Ask the user how many names they would like to generate
+number_of_names = int(input('How many names would you like to generate? '))
+
+# Generate the names        
+used_names = set()
+character_names = [generate_name(used_names) for _ in range(number_of_names)]
+
+# Print the names to the console
+with open('character_names.txt', 'w') as f:
+        for names in character_names:
+            f.write(names + '\n')
+
+print('Names written to file')
